d35e46f73753fd89492ff3eb5e1b32f38ea74c8a
Samuel Knoch <samuel@korntal.eu>
2022-03-28 16:04:08 +0200

    add first contend based recommender service

--- recommender-engine/app.py
+++ recommender-engine/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from src.contendBased import ContendBasedRecommendation
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def get_recommendation():
+    recommender = ContendBasedRecommendation()
+    data = recommender.get_recommendations('SAMSUNG 32" UJ590 Widescreen UHD LED Monitor - LU32J590UQNXZA')
+    return data.to_csv()
---
+++ recommender-engine/src/contendBased.py
@@ -0,0 +1,80 @@
+from sqlite3 import DatabaseError
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+from pymongo.collection import Collection
+
+from .database import get_mongo_client
+
+
+
+class ContendBasedRecommendation():
+
+    product_collection: Collection = None
+    category_collection: Collection = None
+    dataframe: pd.DataFrame = None
+    sim_vector = None
+
+    def __init__(self) -> None:
+        client = get_mongo_client()
+        self.product_collection = client.recommender.product
+        self.category_collection = client.recommender.category
+        self.convert_products_collection_to_df()
+
+        self.create_soup_and_overview()
+        self.vertorize()
+
+    def convert_products_collection_to_df(self) -> None:
+        df_product = pd.DataFrame(list(self.product_collection.find()))
+        df_category = pd.DataFrame(list(self.category_collection.find()))
+
+        for i, row in df_product.iterrows():
+            category = df_category.loc[df_category['_id'] == row['category']]
+            if category is not None:
+                df_product.at[i, 'category'] = category['name']
+        self.dataframe = df_product
+    
+    def create_soup_and_overview(self) -> None:
+        def create_soup(x):
+            # TODO: description (need parsing, currently its html code)
+            #  + '' + ''.join(x['description']) + '' 
+            return ''.join(x['name']) + '' + ''.join(x['category']) + '' + ''.join(str(x['price']))
+
+        # TODO fill nan if collum is not needed
+        # self.dataframe['overview'] = self.dataframe['overview'].fillna('')
+        self.dataframe['soup'] = self.dataframe.apply(create_soup, axis=1)
+
+    def vertorize(self) -> None:
+        tfidf = TfidfVectorizer(stop_words='english')
+        tfidf_matrix = tfidf.fit_transform(self.dataframe['soup'])
+        self.sim_vector = linear_kernel(tfidf_matrix, tfidf_matrix)
+    
+    def get_recommendations(self, productName):
+        if self.sim_vector is None:
+            raise ValueError('sim_vector is empty, should calculate sim. first')
+
+        indices = pd.Series(self.dataframe.index, index=self.dataframe['name']).drop_duplicates()
+
+        idx = indices[productName]
+
+        sim_scores = list(enumerate(self.sim_vector[idx]))
+
+        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+        sim_scores = sim_scores[1:]
+        movie_indices = [i[0] for i in sim_scores]
+
+
+        result_df = pd.DataFrame()
+        result_df['_id'] = ''
+        result_df['name'] = ''
+        result_df['score'] = ''
+        for movie_idices, sim_score in sim_scores:
+            name = self.dataframe['name'].iloc[movie_idices]
+            product_id = self.dataframe['_id'].iloc[movie_idices]
+            data = {'_id': product_id,'name': name, 'score': sim_score}
+            result_df = result_df.append(data, ignore_index=True)
+
+        #self.dataframe['name'].iloc[movie_indices]
+
+        return result_df
